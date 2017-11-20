cb1649d7f059e9b571afe44ab2d9da06ed422781
Djoendhie <ihaandjoe68@Gmail.com>
2017-11-20 09:44:43 +0700

    First

---
+++ TuhfatulAthfal/TuhfatulAthfal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TuhfatulAthfal
+//
+//  Created by Macbook pro on 01/11/17.
+//  Copyright © 2017 Shin Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
